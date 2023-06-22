95b86a606df366a817a9e26b64260b52b5798fec
azprivate web master <cnua@hotmail.com>
2023-06-22 05:03:46 +0100

    duh!

--- cnua.netcore6.crslib_Tests/Entities/Extensions/ChatRoom_ExtensionTests.cs
+++ cnua.netcore6.crslib_Tests/Entities/Extensions/ChatRoom_ExtensionTests.cs
@@ -37,21 +37,6 @@ namespace CSR.Entities.Extensions.Tests
 			Assert.True(expected == actual);
 		}
 
-		[Fact()]
-		public void GetPostsTest()
-		{
-			//arrange
-			string path = @"C:\Users\cnua\source\repos\ChatroomStudioProto\ChatroomStudioProto\cnua.netcore6.crslibTests\Resources\WhatsApp Chat with Andy Obd.txt";
-			var cr = new ChatRecordSet();
-			var fi = new FileInfo(path);
-			cr.Load(fi.FullName);
-			//act
-			var expected = 30;
-			var actual = cr.Posts.Count();
-			//assert
-			Assert.True(expected == actual);
-
-		}
 	}
 }
 
